6d8835f0748866ec8ee8dc26f0569460298bcacd
bacuong312 <bacuong312@gmail.com>
2022-01-03 01:22:31 +0700

    settings + profile ui

---
+++ chat_bot/lib/Model/SettingRequestModel.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+SettingModel settingJson(String str) =>
+    SettingModel.fromJson(json.decode(str));
+class SettingModel {
+  bool allowAutoT2s;
+  bool allowPushNotification;
+  bool allowVoiceRecording;
+  String voiceSelection;
+  double voiceRate;
+
+  SettingModel(
+      {this.allowAutoT2s,
+        this.allowPushNotification,
+        this.allowVoiceRecording,
+        this.voiceSelection,
+        this.voiceRate});
+
+  SettingModel.fromJson(Map<String, dynamic> json) {
+    allowAutoT2s = json['allow_auto_t2s'] != null ? json['allow_auto_t2s'] : null;
+    allowPushNotification = json['allow_push_notification'] != null ? json['allow_push_notification'] : null;
+    allowVoiceRecording = json['allow_voice_recording'] != null ? json['allow_voice_recording'] : null;
+    voiceSelection = json['voice_selection'] != null ? json['voice_selection'] : null;
+    voiceRate = json['voice_rate'] != null ? json['voice_rate'] : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['allow_auto_t2s'] = this.allowAutoT2s;
+    data['allow_push_notification'] = this.allowPushNotification;
+    data['allow_voice_recording'] = this.allowVoiceRecording;
+    data['voice_selection'] = this.voiceSelection;
+    data['voice_rate'] = this.voiceRate;
+    return data;
+  }
+}
